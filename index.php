b55dec4557108e21d77b77f792e8922eda3f22ee
paulineef <960707@live.se>
2017-10-05 14:25:27 +0200

    index

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
 	<title>Förkrök</title>
 </head>
 <body>
- <div>
- 	Det här är vår fina index-fil :)
-</body>
 <?php include ("sidebar.php") ?>
+
+
 <?php include ("footer.php") ?>
+
+<style type="text/css">
+</style>
+
+</body>
 </html>
