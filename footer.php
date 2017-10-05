dec5cbbed5f75f0a4ebb0974dcc07b8dfeb0f90a
Frida <Frida@Fridas-MacBook-Pro.local>
2017-10-05 13:35:39 +0200

    s

--- footer.php
+++ footer.php
@@ -3,10 +3,15 @@
 	<head>
 		<meta name="description" content="describes page"/>
 		<meta charset="utf-8" /> 
-		<link rel="stylesheet" type="css" href="cssfrida.css">
 	</head>
 
 	<footer>
 		<p>Copyright © 2017 forkrok</p>
 	</footer>
 </html>
+
+<style type="text/css">
+	p {
+		color:blue;
+	}
+</style>
