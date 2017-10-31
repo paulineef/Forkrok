bac1f88c8543d76c272bebbcc253e1f2420a5505
lisaarman <lisaarman2@gmail.com>
2017-10-31 13:21:30 +0100

    w

--- sidebar.php
+++ sidebar.php
@@ -4,6 +4,7 @@
 		
 		<link rel="stylesheet" type="text/css" href="forkrok.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
 		<script src="https://use.fontawesome.com/6f2a9fca0c.js"></script>
 		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
 		<script type="text/javascript" charset="utf-8">
