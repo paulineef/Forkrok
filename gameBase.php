d0fd190186975bf9a5e615622f7a3a0af383dd93
Pauline <960707@live.se>
2017-11-06 15:55:21 +0100

    mellan upload

--- gameBase.php
+++ gameBase.php
@@ -10,7 +10,21 @@
 					<th><h2>Name of the Game</h2></th>
 				</tr>
 				<tr>
-					<td><i>What you need:</i></td>
+					<td><i>What you need:</i>
+						<ul>
+							<li>A deck of cards</li>
+							<li>A mug</li>
+						</ul>
+					</td>
+					<td>
+						<h6>Instructions</h6>
+						<p>
+							Sit around a table with your friends. Put a large cup in the middle of the table (a red cup works fine too). Tell your guys and gals to stop snapchatting or texting their exes, because this game only works when people are paying attention. Grab a deck of cards. Each card has an action everyone must follow during the game. Spread the cards around the cup, making a circle shape…still with us? Great, keep reading. The game comes to an end (sadly) when the fourth King is pulled from the deck of cards.
+ 
+							Now that you’re all set up and ready to go, let’s breakdown what each card means, which are shown in the illustration.
+
+						</p>
+					</td>
 				</tr>
 			</table>
 		</div>
